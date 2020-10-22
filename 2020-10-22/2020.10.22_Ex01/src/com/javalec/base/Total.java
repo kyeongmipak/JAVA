28e19b568f21241bbd37c23bcd75b38223ae5b16
kyeongmipak <73284104+kyeongmipak@users.noreply.github.com>
2020-10-22 19:54:13 +0900

    몫이 1이 아니면 반복

--- 2020-10-22/2020.10.22_Ex01/src/com/javalec/base/Total.java
+++ 2020-10-22/2020.10.22_Ex01/src/com/javalec/base/Total.java
@@ -4,12 +4,15 @@ public class Total {
 	//field
 	
 	//constructor
+	public Total() {
+		
 	}
 		
 	//method
 	public void valueTotal(int i) {
 		int total = 0;
 		
+		while(i != 0) { // 몫이 1이 아니면 반복
 			total += i%10;
 			i = i/10;
 		}
